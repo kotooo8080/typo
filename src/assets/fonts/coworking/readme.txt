6ac9358bcaebd34915ad1dbb7d1ff25b4bb8349a
kotooo8080 <kotooo8080@gmail.com>
2023-11-08 23:27:40 +0300

    init

---
+++ src/assets/fonts/coworking/readme.txt
@@ -0,0 +1,4 @@
+Sans serif font for logos and headlines. Free for non-commercial use. 
+Please write to stas@chiganov.ru for commercial use.
+
+https://www.behance.net/chiganov
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path'); // Импортируем модуль "path" для работы с путями файлов
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+let htmlPageNames = ['crocembouche', 'tarttaten'];
+let multipleHtmlPlugins = htmlPageNames.map(name => {
+  return new HtmlWebpackPlugin({
+    template: `./src/${name}.html`,
+    filename: `${name}.html`,
+    chunks: ['main']
+  })
+});
+
+module.exports = {
+  entry: {
+    main: './src/assets/js/index.js'
+  }, // Точка входа для сборки проекта
+
+  output: {
+    filename: 'bundle.js', // Имя выходного файла сборки
+    path: path.resolve(__dirname, 'dist'), // Путь для выходного файла сборки
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(scss|css)$/, // Регулярное выражение для обработки файлов с расширением .css, .scss
+        use: ['style-loader', 'css-loader', 'sass-loader'], // Загрузчики, используемые для обработки CSS/Sass - файлов
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      chunks: ['main']
+    })
+  ].concat(multipleHtmlPlugins),
+
+  devServer: {
+    port: 8080,
+    hot: "only",
+    static: {
+      directory: path.join(__dirname, './'), // Каталог для статики
+      serveIndex: true,
+    },
+    open: true, // Автоматически открывать браузер
+  }, 
+
+  mode: 'development', // Режим сборки
+};
